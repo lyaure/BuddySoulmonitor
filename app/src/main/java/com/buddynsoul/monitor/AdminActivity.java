7acdd1ce501469e0be65c5afa07e983f851f03a3
lyaure <49193419+lyaure@users.noreply.github.com>
2020-06-11 06:24:47 +0300

    add exit dialog for admin activity
    allow admin to switch to monitor

--- app/src/main/java/com/buddynsoul/monitor/AdminActivity.java
+++ app/src/main/java/com/buddynsoul/monitor/AdminActivity.java
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -39,17 +42,43 @@ public class AdminActivity extends FragmentActivity {
                 @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     switch (item.getItemId()) {
                         case R.id.navigation_users:
-                            fragment = new UsersListFragment();
+                            fragment = new UsersFragment();
                             loadFragment(fragment);
                             return true;
 
                         case R.id.navigation_statistics:
-                            fragment = new UsersSearchFragment();
+                            fragment = new StatisticsFragment();
                             loadFragment(fragment);
                             return true;
                     }
                     return false;
                 }
             };
+
+    @Override
+    public void onBackPressed(){
+        new AlertDialog.Builder(this)
+                .setTitle("Exit")
+                .setMessage("Are you sure want to exit?")
+                .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setNegativeButton("Go to my monitor", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .show();
+    }
 
 }
