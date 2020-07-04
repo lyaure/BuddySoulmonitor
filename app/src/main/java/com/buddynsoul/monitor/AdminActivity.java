a033ce27929a682badca4188ddb9bcb2700116c2
lyaure <49193419+lyaure@users.noreply.github.com>
2020-07-04 23:51:06 +0300

    fix stats names in UI

--- app/src/main/java/com/buddynsoul/monitor/AdminActivity.java
+++ app/src/main/java/com/buddynsoul/monitor/AdminActivity.java
@@ -82,4 +82,15 @@ public class AdminActivity extends FragmentActivity {
                 .show();
     }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
 }
